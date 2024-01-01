d4baefe614cd46064773c3a229558a85fb8d1e41
Ayyash <abdul_ayyash@yahoo.com>
2024-01-01 09:05:56 +0300

    quick main

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -15,10 +15,6 @@ export class AppComponent {
   constructor(private auth: Auth) {}
   Logout() {
     // logout and reroute
-    // this.authState.Logout(true);
-    // // also remove redirect
-    // this.authState.redirectUrl = null;
-    // // also local logout if using local server to set cookie
-    // this.authService.Logout().subscribe();
+    
   }
 }
